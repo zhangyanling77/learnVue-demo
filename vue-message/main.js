5fba49f2860e89ef8bf842b124fa4851852ef353
zhangyanling <18783226594@163.com>
2019-10-24 22:37:43 +0800

    message组件封装

---
+++ vue-message/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import App from './App'
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+// Vue.use(ElementUI)
+let vm  = new Vue({
+    el:'#app',
+    render: h => h(App)
+})
