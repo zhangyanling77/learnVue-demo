e5a3392b2a8699ae89435277d75f34b7fe923347
zhangyanling77 <18783226594@163.com>
2019-10-29 21:37:55 +0800

    Update Message.vue

--- vue-message/Message.vue
+++ vue-message/Message.vue
@@ -8,7 +8,6 @@
 
 <script>
 export default {
-    // 每次用户点击按钮时  都是增加数据 自动 渲染视图上
     data(){
         return {layers:[]}
     },
@@ -16,7 +15,7 @@ export default {
         this.id = 0;
     },
     // ref $children
-    methods:{ // 我要提供给外界去掉
+    methods:{ 
         add(options){ // 增加一个序号，时间到了 需要根据序号将他移除掉
             let layer = {...options,id:++this.id};
             this.layers.push(layer);
