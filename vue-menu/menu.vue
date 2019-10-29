5c4cef814cd3e5ca062f78faeb64066f1237a495
zhangyanling77 <18783226594@163.com>
2019-10-29 21:30:41 +0800

    Update menu.vue

--- vue-menu/menu.vue
+++ vue-menu/menu.vue
@@ -1,11 +1,6 @@
 <template>
   <zf-menu>
-      <!-- 1).先循环一次 -->
     <template v-for="menu in data">
-      <!-- <zf-submenu v-if="menu.children" :key="menu.id">
-        <template slot="title">{{menu.name}}</template>
-      </zf-submenu> -->
-      <!-- 2.将有可能重复的项 再次循环 -->
       <Resub v-if="menu.children" :key="menu.id" :data="menu"></Resub>
       <zf-menu-item  v-else :key="menu.id">{{menu.name}}</zf-menu-item>
     </template>
